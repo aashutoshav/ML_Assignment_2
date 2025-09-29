66828bdcf5769ca0060e7ff1d454e0916703b2af
avadoe <f20210056@hyderabad.bits-pilani.ac.in>
2025-09-29 15:42:11 -0400

    yet to fix: gmm.py _E_step

--- gmm.py
+++ gmm.py
@@ -74,17 +74,13 @@ class GMM(object):
         """
         D = mu_i.shape[0]
         variance = np.diagonal(sigma_i) + SIGMA_CONST
-        
-        term1 = -(D / 2.0) * np.log(2 * np.pi)
-        term2 = -0.5 * np.sum(np.log(variance + LOG_CONST))
-        
         centered_pts = points - mu_i
-        term3 = -0.5 * np.sum((centered_pts**2) / variance, axis=1)
-        log_pdf = term1 + term2 + term3
-        log_pdf = np.clip(log_pdf, -500, 500)
+        
+        log_prob_per_dim = -0.5 * np.log(2 * np.pi * variance) - (0.5 * (centered_pts**2 / variance))
+        log_pdf = np.sum(log_prob_per_dim, axis=1)
+        
         pdf = np.exp(log_pdf)
         pdf = np.maximum(pdf, LOG_CONST)
-        
         return pdf
         
 
@@ -103,32 +99,23 @@ class GMM(object):
             3. You may wanna check if the matrix is singular before implementing calculation process.
         """
         D = mu_i.shape[0]
-
         try:
             sigma_reg = sigma_i + np.eye(D) * SIGMA_CONST
+            sign, logdet = np.linalg.slogdet(sigma_reg)
+            if sign != 1:
+                return np.full(points.shape[0], LOG_CONST)
             inv_sigma = np.linalg.inv(sigma_reg)
-            det_sigma = np.linalg.det(sigma_reg)
-            
-            if det_sigma <= 0:
-                det_sigma = LOG_CONST
-            
         except LinAlgError:
-            return np.zeros(points.shape[0])
+            return np.full(points.shape[0], LOG_CONST)
 
-        log_norm_const = -0.5 * D * np.log(2 * np.pi) - 0.5 * np.log(det_sigma + LOG_CONST)
-
+        log_norm_const = -0.5 * D * np.log(2 * np.pi) - 0.5 * logdet
         centered_points = points - mu_i
         mahalanobis_dist = np.sum((centered_points @ inv_sigma) * centered_points, axis=1)
         
-        mahalanobis_dist = np.clip(mahalanobis_dist, 0, 500)
-        
         log_pdf = log_norm_const - 0.5 * mahalanobis_dist
         
-        log_pdf = np.clip(log_pdf, -500, 500)
         pdf = np.exp(log_pdf)
-        
         pdf = np.maximum(pdf, LOG_CONST)
-        
         return pdf
 
     def create_pi(self):
@@ -228,18 +215,12 @@ class GMM(object):
         
         for k in range(self.K):
             if full_matrix:
-                pdf_values = self.multinormalPDF(self.points, mu[k], sigma[k])
+                log_pdf_values = self.multinormalPDF(self.points, mu[k], sigma[k])
             else:
-                pdf_values = self.normalPDF(self.points, mu[k], sigma[k])
-                
-            if pi[k] > 0:
-                log_pi = np.log(pi[k])
-            else:
-                log_pi = -np.inf
-            
-            log_pdf = np.log(pdf_values)
-            
-            ll[:, k] = log_pi + log_pdf
+                log_pdf_values = self.normalPDF(self.points, mu[k], sigma[k])
+
+            log_pi = np.log(pi[k] + LOG_CONST)
+            ll[:, k] = log_pi + log_pdf_values
             
         return ll
 
@@ -257,8 +238,9 @@ class GMM(object):
         Hint:
             You should be able to do this with just a few lines of code by using _ll_joint() and softmax() defined above.
         """
-        joint_ll = self._ll_joint(pi, mu, sigma, full_matrix, **kwargs)
-        tau = self.softmax(joint_ll)
+        ll = self._ll_joint(pi, mu, sigma, full_matrix=full_matrix, **kwargs)
+        tau = self.softmax(ll)
+        
         return tau
 
     def _M_step(self, tau, full_matrix=FULL_MATRIX, **kwargs):
